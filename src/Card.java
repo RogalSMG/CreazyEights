c91f833d861c4e6250c7274b83dd54bf977764d4
Daniel R <danielsmogorzow1@gmail.com>
2022-04-04 11:05:38 +0200

    Initial commit

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,6 @@
 public class Card {
-    private int rank;
-    private int suit;
+    private final int rank;
+    private final int suit;
     public static final String[] RANKS = {null, "as", "2", "3", "4", "5", "6", "7", "8", "9", "10", "walet", "dama", "król"};
     public static final String[] SUITS = {"trefl", "karo", "kier", "pik"};
 
@@ -9,6 +9,11 @@ public class Card {
         this.suit = suit;
     }
 
+    /**
+     * Compare cards, return +1 if given card is lesser, -1 when greater and 0 if equals.
+     * @param card second card
+     * @return int
+     */
     public int compareTo(Card card) {
         if (this.suit > card.suit) {
             return 1;
@@ -23,13 +28,40 @@ public class Card {
     }
 
     /**
-     * Check if given card obj is equal to this.
-     * If are equals return true, else false.
+     * <p>Check if given card obj is equal to this.</p>
+     * If are equals return true, if else false.
      * @param card checked card
      * @return boolean
      */
     public boolean equals(Card card) {
         return (this.rank == card.rank && this.suit == card.suit);
+    }
+
+    public static Card[] makingDeck() {
+        Card[] deck = new Card[52];
+        int ii = 0;
+        for (int i = 0; i < SUITS.length; i++) {
+            for (int j = 0; j < RANKS.length - 1; j++) {
+                deck[ii] = new Card(j,i);
+            }
+        }
+        return deck;
+    }
+
+    /**
+     * Getter to rank
+     * @return int
+     */
+    public int getRank() {
+        return rank;
+    }
+
+    /**
+     * Getter to suit
+     * @return int
+     */
+    public int getSuit() {
+        return suit;
     }
 
     /**
--- src/CardTest.java
+++ src/CardTest.java
@@ -4,6 +4,14 @@ class CardTest {
 
     @org.junit.jupiter.api.Test
     void compareTo() {
+        Card card = new Card(6,1);
+        Card card1 = new Card(5,0);
+        Card card2 = new Card(7,1);
+        assertAll(
+                () -> assertEquals(0,card.compareTo(card)),
+                () -> assertEquals(1,card.compareTo(card1)),
+                () -> assertEquals(-1,card.compareTo(card2))
+        );
     }
 
     @org.junit.jupiter.api.Test

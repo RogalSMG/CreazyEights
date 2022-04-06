2a249cfba5723d3452d1b5b5cae4036ea59b060f
Daniel R <danielsmogorzow1@gmail.com>
2022-04-06 17:46:39 +0200

    New methods Poker.java

--- src/Poker.java
+++ src/Poker.java
@@ -2,6 +2,86 @@
  * This class contain method to compare poker hands
  */
 public class Poker {
+
+    /**
+     * Method check if given array has value 2 under any index.
+     *
+     * @param ranks given array
+     * @return true if found any 2, otherwise false
+     */
+    public static boolean hasPair(int[] ranks) {
+        return wherePair(ranks, 1) >= 0;
+    }
+
+    /**
+     * Method check if given array has value 2 under any index.
+     *
+     * @param ranks given array
+     * @param index int value where method start to search, included
+     * @return index of given array where were value 2 founded,
+     * return -1 if method do not find any 2
+     */
+    private static int wherePair(int[] ranks, int index) {
+        for (int i = index; i < ranks.length; i++) {
+            if (ranks[i] == 2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Method check if given array contains two two values.
+     *
+     * @param ranks given array
+     * @return true if found two pairs, otherwise false
+     */
+    public static boolean hasTwoPairs(int[] ranks) {
+        int nextIndex = 1;
+        for (int i = 0; i < 2; i++) {
+            int pairIndex = wherePair(ranks, nextIndex);
+            if (pairIndex <= 0) {
+                return false;
+            }
+            nextIndex = pairIndex + 1;
+        }
+        return true;
+    }
+
+    /**
+     * Method check if given array has value 3 under any index.
+     *
+     * @param ranks given array
+     * @return true if found any 3 value in array, otherwise false
+     */
+    public static boolean hasTriple(int[] ranks) {
+        for (int i = 1; i < ranks.length; i++) {
+            if (ranks[i] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Method check if given array has value 4 under any index.
+     *
+     * @param ranks given array
+     * @return true if found any 4 value in array, otherwise false
+     */
+    public static boolean hasFours(int[] ranks) {
+        for (int i = 1; i < ranks.length; i++) {
+            if (ranks[i] == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasFullHouse(int[] ranks) {
+        return (hasPair(ranks) && hasTriple(ranks));
+    }
+
     /**
      * Method makes a suits' histogram of given array.
      *
@@ -20,8 +100,8 @@ public class Poker {
     /**
      * Check if in given array is index with value equal or greater than 5.
      *
-     * @param suits given histogram
-     * @return true if number 5 or greater were found in array, otherwise false.
+     * @param suits given array
+     * @return true if number 5 or greater were found in array, otherwise false
      */
     public static boolean hasFlush(int[] suits) {
         for (int suit : suits) {
@@ -37,7 +117,7 @@ public class Poker {
      *
      * @param cards array to make histogram
      * @return array of length of number of ranks where
-     * value under each index represent amount of card of each rank.
+     * value under each index represent amount of card of each rank
      */
     public static int[] rankHist(Card[] cards) {
         int[] hist = new int[Card.RANKS.length];
@@ -47,11 +127,19 @@ public class Poker {
         return hist;
     }
 
+    /**
+     * Check if given array has five consecutive values next to each other.
+     *
+     * @param ranks given array
+     * @return true if there are five consecutive values next to each other,
+     * otherwise false
+     */
     public static boolean hasStraight(int[] ranks) {
         //Straight from ten to ace, needed because ace has rank 1
         if (ranks[1] > 0 && ranks[10] > 0 && ranks[11] > 0 && ranks[12] > 0 && ranks[13] > 0) {
             return true;
         }
+
         int i = 1;
         //find first card
         while (i <= ranks.length - 5) {
@@ -60,6 +148,7 @@ public class Poker {
                 int count = 1;
                 //check next four position (cards) if are present
                 for (int j = i + 1; j < i + 5; j++) {
+                    //when found increase count
                     if (ranks[j] > 0) {
                         count++;
                     } else {
@@ -78,7 +167,7 @@ public class Poker {
     }
 
     /**
-     * Helping method. Finding next bigger than zero value and return its index.
+     * Helping method. Finding next index where value is bigger than zero.
      * Start search from index.
      *
      * @param ranks given array to search
@@ -96,8 +185,15 @@ public class Poker {
         return ranks.length;
     }
 
-
-    public static boolean hasPoker(int[] suits, int[] ranks) {
-        return (hasStraight(ranks) && hasFlush(suits));
+    /**
+     * Method check if given cards array has poker. That is, it has both straight and flush.
+     *
+     * @param cards given array
+     * @return true if given array is poker, otherwise false
+     */
+    public static boolean hasPoker(Card[] cards) {
+        int[] suitsHist = suitHist(cards);
+        int[] rankHist = rankHist(cards);
+        return (hasStraight(rankHist) && hasFlush(suitsHist));
     }
 }
--- src/PokerTest.java
+++ src/PokerTest.java
@@ -25,8 +25,13 @@ class PokerTest {
     Card[] straightAce = {card1, card2, card3, card6, card7};
     Card[] straightTwo = {card2, card3, card6, card7, card9};
     Card[] straightTen = {card10, card11, card12, card13, card1};
-    Card[] straightNine = {card14 ,card10, card11, card12, card13};
+    Card[] straightNine = {card14, card10, card11, card12, card13};
     Card[] poker = {card2, card3, card6, card7, card8};
+    Card[] onePair = {card, card1, card6, card7, card8};
+    Card[] twoPairs = {card8, card9, card1, card4, card14};
+    Card[] triple = {card, card1, card4, card7, card8};
+    Card[] fours = {card, card1, card4, card, card8};
+    Card[] fullHouse = {card, card1, card4, card9, card8};
 
     @Test
     void testSuitHist() {
@@ -53,7 +58,7 @@ class PokerTest {
     }
 
     @Test
-    void hasStraight() {
+    void testHasStraight() {
         assertAll(
                 () -> assertFalse(Poker.hasStraight(Poker.rankHist(cards2))),
                 () -> assertFalse(Poker.hasStraight(Poker.rankHist(cards1))),
@@ -61,6 +66,69 @@ class PokerTest {
                 () -> assertTrue(Poker.hasStraight(Poker.rankHist(straightTwo))),
                 () -> assertTrue(Poker.hasStraight(Poker.rankHist(straightNine))),
                 () -> assertTrue(Poker.hasStraight(Poker.rankHist(straightTen)))
+        );
+    }
+
+    @Test
+    void testHasPoker() {
+        assertAll(
+                () -> assertTrue(Poker.hasPoker(poker)),
+                () -> assertFalse(Poker.hasPoker(straightTwo)),
+                () -> assertFalse(Poker.hasPoker(cards2))
+        );
+    }
+
+    @Test
+    void TestHasPair() {
+        assertAll(
+                () -> assertFalse(Poker.hasPair(Poker.rankHist(poker))),
+                () -> assertFalse(Poker.hasPair(Poker.rankHist(straightTwo))),
+                () -> assertFalse(Poker.hasPair(Poker.rankHist(cards2))),
+                () -> assertTrue(Poker.hasPair(Poker.rankHist(onePair)))
+        );
+    }
+
+    @Test
+    void TestHasTwoPairs() {
+        assertAll(
+                () -> assertFalse(Poker.hasTwoPairs(Poker.rankHist(poker))),
+                () -> assertFalse(Poker.hasTwoPairs(Poker.rankHist(straightTwo))),
+                () -> assertFalse(Poker.hasTwoPairs(Poker.rankHist(cards2))),
+                () -> assertFalse(Poker.hasTwoPairs(Poker.rankHist(onePair))),
+                () -> assertTrue(Poker.hasTwoPairs(Poker.rankHist(twoPairs)))
+        );
+    }
+
+    @Test
+    void testHasTriple() {
+        assertAll(
+                () -> assertFalse(Poker.hasTriple(Poker.rankHist(cards2))),
+                () -> assertFalse(Poker.hasTriple(Poker.rankHist(onePair))),
+                () -> assertFalse(Poker.hasTriple(Poker.rankHist(twoPairs))),
+                () -> assertTrue(Poker.hasTriple(Poker.rankHist(triple)))
+        );
+    }
+
+    @Test
+    void testHasFours() {
+        assertAll(
+                () -> assertFalse(Poker.hasFours(Poker.rankHist(cards2))),
+                () -> assertFalse(Poker.hasFours(Poker.rankHist(onePair))),
+                () -> assertFalse(Poker.hasFours(Poker.rankHist(twoPairs))),
+                () -> assertFalse(Poker.hasFours(Poker.rankHist(triple))),
+                () -> assertTrue(Poker.hasFours(Poker.rankHist(fours)))
+        );
+    }
+
+    @Test
+    void testHasFullHouse() {
+        assertAll(
+                () -> assertFalse(Poker.hasFullHouse(Poker.rankHist(cards2))),
+                () -> assertFalse(Poker.hasFullHouse(Poker.rankHist(onePair))),
+                () -> assertFalse(Poker.hasFullHouse(Poker.rankHist(twoPairs))),
+                () -> assertFalse(Poker.hasFullHouse(Poker.rankHist(triple))),
+                () -> assertFalse(Poker.hasFullHouse(Poker.rankHist(fours))),
+                () -> assertTrue(Poker.hasFullHouse(Poker.rankHist(fullHouse)))
         );
     }
 }
